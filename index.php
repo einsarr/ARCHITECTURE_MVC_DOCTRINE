1666f509c50c4c482abcc07b8544acf48f43e50e
Moussa SARR <einsarr@gmail.com>
2020-01-07 11:46:56 +0000

    Modification du fichier .htaccess

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
+
+use libs\system\Bootstrap;
+
 require_once "config/autoload.php";
-use src\controller\RolesController;
+/*use src\controller\RolesController;
 $roles_dao = new RolesController();
 //$roles_dao->add();
-$roles_dao->getAll();
+$roles_dao->getAll();*/
+
+$mvc = new Bootstrap();
 
